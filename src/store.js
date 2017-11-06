905bcf786e996280c1e7c50073deec306f656b2b
Andy Amaya <aamay001@gmail.com>
2017-11-06 14:24:33 -0800

    redux logger only in dev

--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import reducer from './reducers';
 
+if (process.env.REACT_APP_ENV === 'dev') {
+  var { createLogger } = require('redux-logger');
+}
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middleware = [createLogger(), thunk];
+
+let middleware;
+if (process.env.REACT_APP_ENV === 'dev') {
+  middleware = [createLogger(), thunk];
+} else {
+  middleware = [thunk]
+}
 
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
 const store = createStore(reducer, enhancer);
