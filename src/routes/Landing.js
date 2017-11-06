5023df09188698fd25c937f40f7536d016da70ad
Andy Amaya <aamay001@gmail.com>
2017-11-06 15:03:12 -0800

    text update on landing page

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -15,10 +15,9 @@ export class Landing extends Component {
           Welcome to imNext! imNext allows you to schedule an appointment with
           your personal service provider. No matter if it's your barber, hair
           dresser, mobile carwash, or mobile notary, imNext puts you next in
-          line and reminds you when it's time for your appointment. Ready? Go!
-          Select New Appointment to schedule your appointment, Log In if you're
-          a service provider to access your dashboard, or Sign Up to create a
-          service provider account.
+          line. Ready? Go! Select New Appointment to schedule your appointment,
+          Log In if you're a service provider to access your dashboard, or Sign Up
+          to create a service provider account.
         </p>
         <div className="user-action-buttons">
           <Link to={ROUTES.APPOINTMENT}>New Appointment</Link>
